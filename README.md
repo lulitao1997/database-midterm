4b8b975b689edd75425a5267e274520c55305517
totoro97 <totoro97@outlook.com>
2017-04-17 23:22:08 +0800

    添加变量声明

--- README.md
+++ README.md
@@ -61,3 +61,27 @@ email 改成 username
 ​	课表
 
 ​	课程信息 { 课程描述，花名册(包括给分)。}
+
+# 变量声明
+
+```
+dashboard-coursesavailable.html {
+	后端传给前端：
+	pagenumber: 已选课程列表中的当前页码
+	pageamount: 已选课程列表中的页面数
+	course: 当前页面应该显示出的课程，list类型或set类型 {
+      	内部元素为dict类型
+      	dict{
+      		'cid': 类似于cno，但是不能有'.'，例如1345.01要写成'1234501'
+      		'cells': list类型或set类型，存储占据的课程格子，格式:'1-3'表示星期一的第三节课
+      		'info': tuple(元组)类型(list类型应该也可以?0..0),格式：(cno, cname, tname, 				credit, capacity)
+      	}
+	}
+	cnameincell: 二维list,cnameincell[i][j]表示课程表第i天第j节课的课程名。
+
+	前端传给后端：
+	select：如果存在，返回试图选课的cid
+	search：如果存在，返回cname, cno, tname
+	turnpage：如果存在，返回'next'或'past'或试图turn到的页码
+}
+```
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash, redirect, url_for, session
+from flask import Flask, render_template, flash, redirect, url_for, session, request
 from flask_wtf import Form
 from wtforms import IntegerField, SubmitField
 from wtforms.validators import Required, NumberRange
@@ -19,14 +19,23 @@ def test_dashboard_coursesavailable():
 	computer = dict()
 	computer['cid']=12345
 	computer['cells'] = ['1-1','2-2','3-3']
-	computer['info'] = ('12345', 'computer', 'wang', '1', '100')
+	computer['info'] = ('123435', 'computer', 'wang', '1', '100')
 	computer['done'] = True
 	cs = { 'cid': 123, 'cells': ['1-3','4-5'], 'info': ('123', 'cs', 'wang', '1', '100'), 'done': False }
 	course = [computer, cs]
-	cname = np.ones((20,20))	
-	return render_template('dashboard-coursesavailable.html', course = course, cname = cname)
+	cnameincell = [['hellooooooo' for col in range(15)] for row in range(8)]
+	return render_template('dashboard-coursesavailable.html', pageamount=10, pagenumber=10, course = course, cnameincell = cnameincell)
 
-
+@app.route('/test/dashboard-coursesavailable', methods=['POST'])
+def test_dashboard_coursesavailable_post():
+	if 'select' in request.form:
+		s = request.form['select']
+		return s
+	elif 'search' in request.form:
+		return request.form['cname']
+	else:
+		return redirect('test.login')
+		
 @app.route('/test/dashboard-coursespossessed')
 def test_dashboard_coursespossessed():
 	course = [('computer', 'z201'), ('love','z202')]
@@ -44,4 +53,4 @@ def gologin():
 def login():
 	return render_template('login.html')
 
-app.run(debug = False)
+app.run(debug = True)
